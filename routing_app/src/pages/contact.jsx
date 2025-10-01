9be24a1c06f7c6031443ed74d8528c0f8f320004
GurveerMangat <141556850+GurveerMangat@users.noreply.github.com>
2025-10-01 22:36:04 +0530

    Delete routing_app directory

--- routing_app/src/pages/contact.jsx
+++
@@ -1,9 +0,0 @@
-// pages/Contact.jsx
-export default function Contact() {
-  return (
-    <div className="text-center mt-10">
-      <h1 className="text-4xl font-bold text-purple-600">📞 Contact Page</h1>
-      <p className="mt-4 text-lg text-gray-700">Get in touch with us through this Contact page.</p>
-    </div>
-  );
-}
